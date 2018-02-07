41c8dbb5b1a1cacce2a2af2ba718fe687fa2fe28
Rene Monroy <rene.dev.js@gmail.com>
2018-02-07 16:19:35 -0600

    Add more paths to configuration

--- config/index.js
+++ config/index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const pathsJSON = require('./paths.json');
 const config = module.exports;
 
+const MAIN = 'main';
+const RENDERERS = 'renderers';
+
 const INDEX_CSS = 'index.css';
 const INDEX_HTML = 'index.html';
 const INDEX_JS = 'index.js';
@@ -41,23 +44,33 @@ config.setup = (options = {}) => (
         config.paths[name] = path.resolve(config.cwd, pathsJSON[name]);
       });
       
-      renderersPath = config.paths.renderers;
+      config.paths.main = {};
+      config.paths.main.root = path.resolve(config.paths.src, MAIN);
+      config.paths.main.js = path.resolve(config.paths.main.root, INDEX_JS);
+
+      config.paths.renderers = {};
+      config.paths.renderers.root = path.resolve(config.paths.src, RENDERERS);
+
+      renderersPath = config.paths.renderers.root;
+
       fs
         .readdirSync(renderersPath)
         .filter(name => (
-          fs.statSync(`${renderersPath}/${name}`).isDirectory() && !name.startsWith('_')
+          fs.statSync(`${renderersPath}/${name}`).isDirectory() &&
+          !name.startsWith('_') &&
+          name !== 'root'
         ))
         .forEach(name => {
-          const pathname = `${name}Renderer`;
           const rendererPath = path.resolve(renderersPath, name);
-          config.renderers[name] = { pathname, active: false };
-          config.paths[pathname] = {
+          config.renderers[name] = { active: false };
+          config.paths.renderers[name] = {
             root: rendererPath,
             css: path.resolve(rendererPath, INDEX_CSS),
             html: path.resolve(rendererPath, INDEX_HTML),
             js: path.resolve(rendererPath, INDEX_JS),
           };
         });
+
       return resolve(config);
     } catch (err) {
       return reject(err);
