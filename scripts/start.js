b8bc23f8178200a81e54e680b6c2a83c5341a04a
Rene Monroy <rene.dev.js@gmail.com>
2018-02-07 15:08:21 -0600

    Create initial file for script start

---
+++ scripts/start.js
@@ -0,0 +1,40 @@
+process.env.NODE_ENV = 'development';
+
+const options = require('minimist')(process.argv.slice(2));
+const config = require('../config/start.config');
+const log = require('../lib/log');
+const lifespan = require('../lib/lifespan');
+const { displayName, version } = require('../package.json');
+
+lifespan.start();
+process.on('unhandledRejection', lifespan.fail('Unhandled error thrown.'));
+
+log.debugging = Boolean(options.debug);
+log.clear();
+log.sign(displayName, version, { font: 'Big' });
+
+// Need an async function to use 'await', a self-invoked function does the job
+(async (opts) => {
+  try {
+
+    // 1. Setup configuration asynchronously and fail if errors are found
+    await config.setup(opts)
+      .catch(lifespan.fail('Error while setting up configuration.'));
+    
+    // ['SIGINT', 'SIGTERM'].forEach((sig) => {
+    //   process.on(sig, () => {
+    //     lifespan.finish();
+    //   });
+    // });
+
+    // console.log(JSON.stringify(config, null, 2));
+    log.debug('Config: ↴\n', JSON.stringify(config, null, 2));
+    lifespan.finish();
+  } catch (err) {
+    if (err && err.message) {
+      return Promise.reject(err.message);
+    }
+    process.exit(1);
+  }
+})({ cwd: process.cwd(), ...options })
+  .catch(lifespan.fail('Something went wrong during start execution.'));
